7405b92451586f0ebd0e1335fe61c08aba8ae532
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-02-11 00:02:20 +0500

    get_first_row.py

--- get_first_row.py
+++ get_first_row.py
@@ -6,7 +6,11 @@ def get_first_row(data):
     Return:
         list: First row.
    """
-   
-   return
+   a=data.split('\n')
+   s=a[1]
+   x=s.split(',')
+   return x
+data=open("data.csv").read()
+print(get_first_row(data))
 
 # Read the csv file
